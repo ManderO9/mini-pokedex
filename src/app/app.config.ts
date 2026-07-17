cfa7b0c787b91072bab78a50c7f16dce87a3f4a3
ManderO9 <hossem.chetoui@gmail.com>
2026-07-17 17:45:36 +0100

    chore: add echarts and ngx-lottie

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,39 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
+import { provideHttpClient } from "@angular/common/http";
+import { provideAnimationsAsync } from "@angular/platform-browser/animations/async";
+import { provideLottieOptions } from "ngx-lottie";
+import { provideEchartsCore } from "ngx-echarts";
+import * as echarts from "echarts/core";
+import { LineChart } from "echarts/charts";
+import {
+GridComponent,
+TooltipComponent,
+LegendComponent,
+} from "echarts/components";
+
+import { CanvasRenderer } from "echarts/renderers";
+
+echarts.use([
+LineChart,
+GridComponent,
+TooltipComponent,
+LegendComponent,
+CanvasRenderer,
+]);
+
+
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient(),
+    provideAnimationsAsync(),
+    provideLottieOptions({ player: () => import("lottie-web") }),
+    provideEchartsCore({ echarts })
   ]
 };
+
